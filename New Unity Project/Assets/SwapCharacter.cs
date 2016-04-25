5b1e0480b846991f20926f5bfe7cdcc61ecc9408
Corey Chilton <simfrek@gmail.com>
2016-04-25 09:34:45 +0100

    week 1
    all work from week 1

---
+++ New Unity Project/Assets/SwapCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapCharacter : MonoBehaviour {
+
+	private bool CurrentCharacter = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("swap") < -0.2) 
+		{
+			light.enabled = !light.enabled;
+//			torchOn = true;
+//			time = 15;
+		}
+	}
+}
---
+++ New Unity Project/Assets/fixController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fixController : MonoBehaviour {
+	private int timer = 0;
+	// Use this for initialization
+	void Start () {
+		var script = GetComponent <ThirdPersonController>();
+		script.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer >= 100) {
+			GetComponent <ThirdPersonController>().enabled = false;
+			enabled = false;
+		}
+		timer++;
+	}
+}
